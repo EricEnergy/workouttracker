9f8dbd64a47ae04f31b97ac75ee6f6a04af54b26
Eric Energy <ericnrgnash@gmail.com>
2020-01-08 12:37:33 -0600

    clean up

--- server.js
+++ server.js
@@ -23,57 +23,6 @@ require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
 
 
-// db.Workout.create({ name: "Fitness tracker" })
-//     .then(Workout => {
-//         console.log(Workout);
-//     })
-//     .catch(({ message }) => {
-//         console.log(message);
-//     });
-
-// app.get("/notes", (req, res) => {
-//     db.Note.find({})
-//         .then(dbNote => {
-//             res.json(dbNote);
-//         })
-//         .catch(err => {
-//             res.json(err);
-//         });
-// });
-
-// app.get("/user", (req, res) => {
-//     db.User.find({})
-//         .then(dbUser => {
-//             res.json(dbUser);
-//         })
-//         .catch(err => {
-//             res.json(err);
-//         });
-// });
-
-// app.post("/submit", ({ body }, res) => {
-//     db.Note.create(body)
-//         .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-//         .then(dbUser => {
-//             res.json(dbUser);
-//         })
-//         .catch(err => {
-//             res.json(err);
-//         });
-// });
-
-// app.get("/populateduser", (req, res) => {
-//     db.User.find({})
-//         .populate("notes")
-//         .then(dbUser => {
-//             res.send(dbUser);
-//         })
-//         .catch(err => {
-//             res.send(err);
-//         });
-// });
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
